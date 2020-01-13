8f92bdde6901ce96fd99c8e84909b931ae8c9be8
GA-CyberWorkforceAcademy <43207128+GA-CyberWorkforceAcademy@users.noreply.github.com>
2020-01-13 16:39:45 +0000

    Add files via upload

---
+++ SO_edits/index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Security Onion: HOME</title>
+</head>
+<body style="background-color:#A3C3E0">
+<center> 
+<br> 
+<b> 
+<a href="https://securityonion.net"><img STYLE="border: none;" src="securityonion_logo.jpg" alt="Security Onion"></img></a><br>
+<a href="https://securityonion.net">https://securityonion.net</a><br> 
+<a href="https://securityonionsolutions.com">https://securityonionsolutions.com</a><br> 
+</b> 
+</center>
+<br> 
+<a href= "/Intel"> For CyberDawg Intel Click Here</a><br> 
+<br>
+<b>Where do I find documentation?</b><br>
+If you have Internet access, please use the <a href="https://securityonion.net/docs">online documentation</a>.<br>
+If you have don't have Internet access, you can access the <a href="/docs">offline documentation</a>.<br>
+(The <a href="https://securityonion.net/docs">online documentation</a> is updated more frequently than the <a href="/docs">offline documentation</a>.)<br>
+<br>
+<b>Need a cheat sheet?</b><br>
+<a href="Security-Onion-Cheat-Sheet.pdf">Cheat Sheet</a><br>
+<br>
+<b>Tools</b><br>
+* <a href="/cyberchef/cyberchef.htm">CyberChef</a>: The Cyber Swiss Army Knife - a web app for encryption, encoding, compression and data analysis<br>
+<?php if (file_exists("/etc/nsm/servertab")): ?>
+	* <a href="/squert/">Squert</a>: View and categorize NIDS/HIDS alerts<br>
+	* <a href="/app/kibana">Kibana</a>: Search logs (IDS, Bro, and syslog) stored in Elasticsearch<br>
+<?php else: ?>
+(More tools will be available here after you run Setup.)<br>
+<?php endif; ?>
+<br>
+<b>Security Onion Solutions</b><br>
+Interested in training, professional services, or hardware appliances?<br>
+<a href="https://securityonionsolutions.com">https://securityonionsolutions.com</a><br> 
+<br> 
+<b>Disclaimer of Warranty<br>
+<small>THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM .AS IS. WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.</small><br>
+<br>
+<b>Limitation of Liability</b><br>
+<small>IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</small><br>
+</body>
+</html>
+
